7b2e23b4b4d32941c7cbdd5efeb8322fbde23bf0
carolinjcastro <109469983+carolinjcastro@users.noreply.github.com>
2025-03-19 19:35:44 -0400

    Update OnlineStore.java

--- OnlineStoreShoppingSystem/src/OnlineStore.java
+++ OnlineStoreShoppingSystem/src/OnlineStore.java
@@ -1,6 +1,5 @@
 /****************************************************************
- *  Carolin Castro, cjc494@rutgers.edu
- *  CS 102 (section 06) - Final Homework
+ *  Julie Castro, cjc494@rutgers.edu
  *  [Creating an "OnlineStore" class that manages a list of 
  *  products and user shopping carts]
  ****************************************************************/
